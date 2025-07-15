a7fe4350a5b18e6e6e2d6b19d66384dd6215c956
pursvir <i-have@no.email>
2025-07-15 13:48:49 +0300

    Complemented file types being scanned, added support for deleting files with \n

--- baloo-clean.sh
+++ baloo-clean.sh
@@ -1,17 +1,37 @@
 #!/usr/bin/env bash
 
-balooctl6 disable
 declare -a file_types=(
+  "Archive"
   "Audio"
-  "Documents"
+  "Video"
+  "Folder"
+  "Document"
   "Media"
-  "Images"
+  "Image"
+  "Text"
 )
+
+contains_newline_chars=0
 for file_type in "${file_types[@]}"; do
   baloosearch6 "type:$file_type" | head -n -1 | while read -r filename; do
-    if [ ! -f "$filename" ]; then
-      balooctl6 clear "$filename"
+    if [[ -z "$filename" && contains_newline_chars -eq 0 ]]; then
+      continue
+    fi
+    if [[ ! -e "$filename" ]]; then
+      if [ $contains_newline_chars -eq 1 ]; then
+        filename_being_deleted+=$'\n'
+        filename_being_deleted+="$filename"
+      else
+        filename_being_deleted="$filename"
+      fi
+      attempt=$(balooctl6 clear "$filename_being_deleted")
+      echo "$attempt"
+      if [[ $attempt == "File not found on filesystem or in DB"* ]]; then
+        contains_newline_chars=1
+      else
+        contains_newline_chars=0
+        filename_being_deleted=""
+      fi
     fi
   done
 done
-balooctl6 enable
